41217dc0d7d2f5cb516f502fd562b518fe854c29
Chris Manson <chris@manson.ie>
2016-04-07 11:12:24 +0100

    adding openConnection function

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var connectMongoose = require('./lib/connectMongoose');
 
-module.exports = {
-  connectCommonMongo: connectMongoose.connectCommonMongo,
-  connectNewMongo: connectMongoose.connectNewMongo,
-  getConnection: connectMongoose.getConnection,
-  connectGlobalMongo: connectMongoose.connectGlobalMongo,
-};
+module.exports = connectMongoose;
