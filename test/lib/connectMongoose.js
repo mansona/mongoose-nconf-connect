c7911a965f08173dd8e433609c5d21ba8b59e543
Chris Manson <chris@stonecircle.io>
2018-02-26 08:58:56 +0000

    making sure username and password are obscured from connectionstring log

--- test/lib/connectMongoose.js
+++ test/lib/connectMongoose.js
@@ -51,13 +51,53 @@ describe('connect mongoose', () => {
     });
   });
 
-  it.skip('obscures the username@password from connection log', () => {
+  it('obscures the username@password from connection log when it is passed with object config', () => {
     nconf.set('mongo', {
       db: 'mySuperDatabase',
       hosts: ['localhost'],
       port: 27017,
       user: 'superface',
       password: 'superpassword',
+    });
+
+    const infoStub = sinon.stub();
+
+    const connection = mongooseConnect.connectNewMongoSync(nconf, mongoose, {
+      logger: {
+        info: infoStub,
+        error: console.error,
+      },
+    });
+    const faces = connection.collection('faces');
+    return Q.ninvoke(faces, 'find').then(() => {
+      sinon.assert.calledWith(infoStub, 'Mongoose connected', 'mongodb://<username_redacted>:<password_redacted>@localhost:27017/mySuperDatabase');
+    });
+  });
+
+  it('obscures the username@password from connection log when it is passed with mixture of object config and connectionString', () => {
+    nconf.set('mongo', {
+      connectionString: 'mongodb://localhost:27017/mySuperDatabase',
+      user: 'superface',
+      password: 'superpassword',
+    });
+
+    const infoStub = sinon.stub();
+
+    const connection = mongooseConnect.connectNewMongoSync(nconf, mongoose, {
+      logger: {
+        info: infoStub,
+        error: console.error,
+      },
+    });
+    const faces = connection.collection('faces');
+    return Q.ninvoke(faces, 'find').then(() => {
+      sinon.assert.calledWith(infoStub, 'Mongoose connected', 'mongodb://<username_redacted>:<password_redacted>@localhost:27017/mySuperDatabase');
+    });
+  });
+
+  it('obscures the username@password from connection log when it is passed with connectionString only', () => {
+    nconf.set('mongo', {
+      connectionString: 'mongodb://superface:superpassword@localhost:27017/mySuperDatabase',
     });
 
     const infoStub = sinon.stub();
