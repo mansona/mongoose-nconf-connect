0b40b8d175b462a8902872ed71c961a90ccbf902
Chris Manson <chris@manson.ie>
2019-06-13 15:26:34 +0100

    fixing strang behaviour (bugfix)

--- test/lib/buildConnectionString.js
+++ test/lib/buildConnectionString.js
@@ -36,18 +36,6 @@ describe('buildConnectionString', () => {
     expect(buildConnectionString.sync(nconf, 'mongo:')).to.eql('mongodb://localhost:27017/mySuperDatabase,outerhost:27017/mySuperDatabase');
   });
 
-  it('builds connection string properly when connectionString is passed without protocol prefix', () => {
-    nconf.set('mongo', {
-      connectionString: 'andnowfor:20771/something,completely:222222/different',
-      user: 'face',
-      password: 'pw',
-      db: 'mySuperDatabase',
-      hosts: ['localhost', 'outerhost'],
-      port: 27017,
-    });
-    expect(buildConnectionString.sync(nconf, 'mongo:')).to.eql('mongodb://face:pw@andnowfor:20771/something,completely:222222/different');
-  });
-
   it('builds connection string properly when full connectionString is passed', () => {
     nconf.set('mongo', {
       connectionString: 'mongodb://andnowfor:20771/something,completely:222222/different',
